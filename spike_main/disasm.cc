a58ec8d3321854446a19d71c7b97f6c739b0a024
patricoferris <patrickferris17@gmail.com>
2020-01-25 15:22:27 +0000

    Add bit manipulation subset

--- spike_main/disasm.cc
+++ spike_main/disasm.cc
@@ -395,6 +395,11 @@ disassembler_t::disassembler_t(int xlen)
   DEFINE_ITYPE(slti);
   DEFINE_ITYPE(cii);
   DEFINE_ITYPE(cgt);
+  DEFINE_RTYPE(andn);
+  DEFINE_RTYPE(orn);
+  DEFINE_RTYPE(xorn);
+  DEFINE_RTYPE(max);
+  DEFINE_RTYPE(min);
   add_insn(new disasm_insn_t("seqz", match_sltiu | match_imm_1, mask_sltiu | mask_imm, {&xrd, &xrs1}));
   DEFINE_ITYPE(sltiu);
   add_insn(new disasm_insn_t("not", match_xori | mask_imm, mask_xori | mask_imm, {&xrd, &xrs1}));
