acc4cef756d3d9957c755dec6484dd192bcef687
patricoferris <patrickferris17@gmail.com>
2020-04-26 15:02:03 +0100

    Read RA register

--- riscv/insns/ocfet.h
+++ riscv/insns/ocfet.h
@@ -1,4 +1,4 @@
 require_rv64;
 sreg_t stack_pointer = sext_xlen(RS1 + insn.i_imm()); 
 WRITE_RD(stack_pointer);
-MMU.store_uint64(stack_pointer + (-insn.i_imm() - 8), X_RA);
+MMU.store_uint64(stack_pointer + (-insn.i_imm() - 8), READ_REG(X_RA));
