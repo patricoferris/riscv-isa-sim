21a7a2af845d73ddede00ff92d0e8d5acba69c87
patricoferris <patrickferris17@gmail.com>
2020-04-26 14:23:16 +0100

    Use sreg_t

--- riscv/insns/ocfet.h
+++ riscv/insns/ocfet.h
@@ -1,4 +1,4 @@
 require_rv64;
-reg_t stack_pointer = sext_xlen(RS1 + insn.i_imm()); 
+sreg_t stack_pointer = sext_xlen(RS1 + insn.i_imm()); 
 WRITE_RD(stack_pointer);
 MMU.store_uint64(stack_pointer + (-insn.i_imm() - 8), X_RA);
