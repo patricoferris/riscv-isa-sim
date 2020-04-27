07af1ecd55ebccc0dc69a58260c96ef715646cf5
patricoferris <patrickferris17@gmail.com>
2020-04-27 10:08:17 +0100

    Fix ordering of ocfet

--- riscv/insns/ocfet.h
+++ riscv/insns/ocfet.h
@@ -1,4 +1,6 @@
 require_rv64;
 sreg_t stack_pointer = sext_xlen(RS1 + insn.i_imm()); 
-WRITE_RD(stack_pointer);
+// Store first in case of fault and rerunning of instruction
 MMU.store_uint64(stack_pointer + (-insn.i_imm() - 8), READ_REG(X_RA));
+// Only then update the stack pointer!
+WRITE_RD(stack_pointer);
